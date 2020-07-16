693ad340ef829c837d3068521072f71e26135c5d
EmilKaczmarski <51459339+EmilKaczmarski@users.noreply.github.com>
2020-07-16 17:45:37 +0200

    added singup class

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,147 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(
+  SignUpApp(),
+);
 
-class MyApp extends StatelessWidget {
+class SignUpApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/': (context) => SignUpScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class SignUpScreen extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    void _signInButtonPressed() {
+    }
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Container(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 24.0),
+                  child: Text(
+                    'Sign up',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 40,
+                    ),
+                  )),
+              SignUpForm(),
+              FlatButton(
+                padding: EdgeInsets.all(10),
+                onPressed: null,
+                child: Container(
+                  decoration: new BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Colors.blueGrey[100],
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text('Sign up'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 14, 0, 0),
+                child: Text("Already registered?",
+                  style: TextStyle(
+                      color: Colors.grey[500]
+                  ),
+                ),
+              ),
+              FlatButton(
+                child: Text("sing in"),
+              )
+            ]),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SignUpForm extends StatefulWidget {
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _SignUpFormState extends State<SignUpForm> {
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController = TextEditingController();
+  final _usernameTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+            child: TextFormField(
+              controller: _firstNameTextController,
+              decoration: InputDecoration(
+                hintText: 'First name',
+                hintStyle: TextStyle(color: Colors.grey[400]),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
+                ),
+              ),
+              style: TextStyle(
+                color: Colors.blueGrey[600],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+            child: TextFormField(
+              controller: _lastNameTextController,
+              decoration: InputDecoration(
+                hintText: 'Last name',
+                hintStyle: TextStyle(color: Colors.grey[400]),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
+                ),
+              ),
+              style: TextStyle(
+                color: Colors.blueGrey[600],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+            child: TextFormField(
+              controller: _usernameTextController,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                hintStyle: TextStyle(color: Colors.grey[400]),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
+                ),
+              ),
+              style: TextStyle(
+                color: Colors.blueGrey[600],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
